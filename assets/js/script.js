98aa82d47a62de4dc3b7759af9dd05d24edb3e8d
Achraf <chibane.tkd04@gmail.com>
2022-12-22 12:15:40 -0700

    added comment to the script.js for Pexels API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,37 @@
-// Pexxels API to call for pictures 
+// API key for accessing Pexels API
 const apiKey = '563492ad6f917000010000012267cfab9210494795b49e30ef07c8bd';
 
 class photoImages {
     constructor() {
+        // API key for accessing Pexels API
         this.apiKey = "563492ad6f917000010000012267cfab9210494795b49e30ef07c8bd";
+        // DOM element to display the images
         this.imagesDiv = document.querySelector('.images');
+        // DOM element for the search form
         this.Search = document.querySelector('.header form');
-        this.Load = document.querySelector('.Load');
+        // Add event listeners
         this.eventHandle();
     }
     eventHandle() {
+        // Fetch and display images when the DOM is loaded
         document.addEventListener('DOMContentLoaded',()=> {
             this.getImg();
         });
-        this.Search.addEventListener('submit', (e)=>{
-            this.getSearch(e);
+        // Fetch and display images based on search input
+        this.Search.addEventListener('submit', (event)=>{
+            this.getSearch(event);
 
         });
     }
+    // Fetch and display curated images from Pexels API
     async getImg() {
         const URL = 'https://api.pexels.com/v1/curated?per_page=12';
         const data = await this.fetchImages(URL);
         this.GenerateHTML(data.photos);
         console.log(data)
     }
+
+    // Fetch images from Pexels API using the API key and authorization header
     async fetchImages(URL) {
         const response = await fetch(URL, {
             method: 'GET',
@@ -35,6 +43,8 @@ class photoImages {
         const data = await response.json();
         return data;
     }
+
+    // Generate HTML elements to display the photos and their details
     GenerateHTML(photos) {
         photos.forEach(photo=> {
             const item = document.createElement('div');
@@ -49,17 +59,26 @@ class photoImages {
         });
 
     }
-    async getSearch(e) {
-        e.preventDefault();
+
+    // Fetch and display images based on search input
+    async getSearch(event) {
+        // Prevent the default submit action
+        event.preventDefault();
+        // Clear the previous search results
         this.imagesDiv.innerHTML = ''
-        const searchValue = e.target.querySelector('input').value;
+        // Get the search input value
+        const searchValue = event.target.querySelector('input').value;
+        // Construct the API URL with the search query
         const URL = await `https://api.pexels.com/v1/search?query=${searchValue}&per_page=12`
+        // Fetch and display the search results
         const data = await this.fetchImages(URL);
         this.GenerateHTML(data.photos)
-        e.target.reset();
+        // Reset the search form
+        event.target.reset();
     }
 }
 
+// Create a new instance of the photoImages class
 const images = new photoImages;
 
 // Added Calander API Here
